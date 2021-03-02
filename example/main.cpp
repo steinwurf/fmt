530b22a2cba4881eabb98ec46410d761faa2f98b
Morten V. Pedersen <morten@mortenvp.com>
2021-03-02 15:11:32 +0100

    Working on it

--- example/main.cpp
+++ example/main.cpp
@@ -1,9 +1,15 @@
 #include <fmt/core.h>
 #include <fmt/ranges.h>
+#include <iostream>
 #include <vector>
 
 int main() {
   fmt::print("Hello, world!\n");
   std::vector<int> v = {1, 2, 3};
   fmt::print("{}\n", v);
+
+  fmt::memory_buffer out;
+  format_to(out, "For a moment, {} happened.", "nothing");
+  std::cout << out.data()
+            << std::endl; // returns a pointer to the formatted data
 }
